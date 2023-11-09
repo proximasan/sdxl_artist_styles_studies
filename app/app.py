38d2f3ac263bc5e2c8e6766289b658c547d13d94
Pablo González <gonzrubio.pablo@gmail.com>
2023-11-09 20:41:00 +0100

    ✨ Add static files and download grid file on startup.

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from litestar.template.config import TemplateConfig
 from litestar.static_files import StaticFilesConfig
 
 from app.endpoints import router
+from app.utils import download_grid_file
 
 # Static path is on ../static relative to this file
 static_path = Path(__file__).parent.parent / "static"
@@ -17,4 +18,5 @@ app = Litestar(
         engine=JinjaTemplateEngine,
     ),
     static_files_config=[StaticFilesConfig(path="/static", directories=[static_path])],
+    on_startup=[download_grid_file],
 )
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,5 @@
 from functools import cache
 import json
-from operator import index
 from pathlib import Path
 import unicodedata
 import os
@@ -59,7 +58,9 @@ def get_artist_tags() -> Tuple[dict[str, list[str]], set[str]]:
                 # Add alternative artist names with a space instead of a hyphen
                 if len(row) >= 7:
                     alternative_name = artist_name.replace("-", " ")
-                    alternative_name = normalize_nfc(alternative_name)  # Normalize alternative name
+                    alternative_name = normalize_nfc(
+                        alternative_name
+                    )  # Normalize alternative name
                     tags_column = row[7] if len(row) > 7 else ""
                     if alternative_name not in artist_tags:
                         artist_tags[alternative_name] = [
@@ -97,7 +98,9 @@ def get_images_data(
             normalized_filename.endswith(".jpg") or normalized_filename.endswith(".png")
         ) and "_0" in normalized_filename:
             artist_name = (
-                " ".join(normalized_filename.split("_")[:-1]).replace("-", " ").replace("  ", " ")
+                " ".join(normalized_filename.split("_")[:-1])
+                .replace("-", " ")
+                .replace("  ", " ")
             )
             artist_name = normalize_nfc(artist_name)  # Normalize artist name
             prompt_index = normalized_filename.split("_")[-1].split(".")[0]
@@ -123,20 +126,28 @@ def get_images_data(
     if tag:
         if tag == "#unrecognized":
             images_data = [
-                image for image in images_data if image.artist_name in unrecognized_artists
+                image
+                for image in images_data
+                if image.artist_name in unrecognized_artists
             ]
         else:
             tag = tag.lower().strip()  # Clean and format the tag for comparison
             images_data = [
-                image for image in images_data if any(t.lower().strip() == tag for t in image.tags)
+                image
+                for image in images_data
+                if any(t.lower().strip() == tag for t in image.tags)
             ]
             images_data = [
-                image for image in images_data if image.artist_name not in unrecognized_artists
+                image
+                for image in images_data
+                if image.artist_name not in unrecognized_artists
             ]
     elif sort_by != "recent" and not search_query:
         # Filter out unrecognized artists only if no tag is provided and it's not a recent sort and no search query is present
         images_data = [
-            image for image in images_data if image.artist_name not in unrecognized_artists
+            image
+            for image in images_data
+            if image.artist_name not in unrecognized_artists
         ]
 
     if letter:
@@ -164,4 +175,25 @@ def get_total_image_count_from_json() -> int:
 
 
 def filter_by_artist(search_query, images_data):
-    return [image for image in images_data if search_query.lower() in image.artist_name.lower()]
+    return [
+        image
+        for image in images_data
+        if search_query.lower() in image.artist_name.lower()
+    ]
+
+
+def check_grids_folder_exists():
+    return Path("static/grids/").resolve().exists()
+
+
+def download_grid_file():
+    # git clone https://huggingface.co/datasets/parrotzone/sdxl-1.0and change
+    # the path /static/grids/ in app.py line 72 to the grids path on your disk,
+    # so that you can just easily do git pull for every new addition
+    if not check_grids_folder_exists():
+        Path("static/grids/").resolve().mkdir(parents=True, exist_ok=True)
+        from huggingface_hub import snapshot_download
+
+        snapshot_download(
+            repo_id="parrotzone/sdxl-1.0", repo_type="dataset", local_dir="static/"
+        )
